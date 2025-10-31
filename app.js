7dbd892817eb3806e7b506926482ebd98c468684
aysenurgezgin <aysenurgezgin99@gmail.com>
2025-10-31 18:54:37 +0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 "use strict";
+const txtTaskDescription =document.getElementById("txt-task-description");
+const btnAddTask =document.getElementById("btn-add-task");
+const taskList = document.getElementById("task-list");
+
+let taskListArray =[
+    {id: 1, taskDescription}
+];
